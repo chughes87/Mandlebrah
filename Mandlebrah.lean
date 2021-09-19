7add7326573ce2ca03f9ff4ffdae44b00a0f185d
Eric Fode <ericfode@gmail.com>
2021-09-19 15:28:55 -0700

    adding theorems to solve

--- Mandlebrah.lean
+++ Mandlebrah.lean
@@ -4,25 +4,54 @@ def step (c z : Complex) : Complex := z ^ 2 + c
 
 def mandel (c : Complex) (bound : Float := 2.0) (maxSteps : Nat := 20) : Nat :=
   let rec iterate : Complex → Nat → Nat → Nat := λ  
-  | _, Nat.zero, _ => 0
-  | z, Nat.succ maxSteps, currStep => 
-    match (Complex.Abs z ≥ bound : Bool) with
-      | true => currStep
-      | false => iterate (step c z) maxSteps currStep.succ 
+  | _, 0, _ => 0
+  | z, maxSteps + 1, currStep => 
+      match (Complex.Abs z ≥ bound : Bool) with
+        | true => currStep
+        | false => iterate (step c z) maxSteps currStep.succ 
   iterate 0 maxSteps 0
 
-def range (default: a) : Array a → Nat → Array a 
-  | acc, 0 => acc
-  | acc, Nat.succ x => range default (acc.append #[default]) x
-
 def twoDeeRange (default: a) (x y: Nat) : Array (Array a) :=
-  range (range default #[] x) #[] y
+  Array.mkArray y <| Array.mkArray x default
 
 def buildDimToComplex (xMax yMax : Nat) (cMin cMax : Complex) : Nat → Nat → Complex :=
   let xScalar : Float := (cMax.real - cMin.real) / Float.ofNat xMax
   let yScalar : Float := (cMax.i - cMin.i) / Float.ofNat yMax
-  λ (px :Nat) => λ (py: Nat) =>
-    cMin + Complex.mk ((Float.ofNat px) * xScalar) ((Float.ofNat py) * yScalar)
+  fun (px py : Nat) => cMin + Complex.mk ((Float.ofNat px) * xScalar) ((Float.ofNat py) * yScalar)
+
+section x
+  variable (size : Nat) 
+  variable (hpx hpy: Nat) 
+  variable (hcMin hcMax : Complex) 
+  variable (hp: hpx < size ∧  hpy < size ∧ hpx >0 ∧ hpy > 0 )
+  variable (hc: hcMin < hcMax) 
+  variable (hsize: size > 0)
+  def ap: Nat → Nat → Complex := 
+    (buildDimToComplex  size size hcMin hcMax) 
+
+#check ap
+
+  theorem BDimIdAtZero : hcMin = (buildDimToComplex size size hcMin hcMax 0 0 ) := by
+    apply Eq.symm
+    sorry
+
+  theorem existsAIncrement : ∃ x : Float, ∀ p : Nat, ∀ q: Nat,  
+                                            (buildDimToComplex size size hcMin hcMax p q) -
+                                            (buildDimToComplex size size hcMin hcMax (p+1) (q+1)) * x = 0 :=
+                                          
+                                            sorry
+ 
+
+  theorem DimInBounds : hcMin < (buildDimToComplex size size hcMin hcMax hpy hpx) ∧ 
+             hcMax > (buildDimToComplex size size hcMin hcMax hpy hpx) := by sorry
+ 
+ #check size < hpx
+--  theorem dimToComplexBounded : hcomp := by
+
+
+  sorry
+
+end x
 
 def newScene (px py : Nat) (cMin cMax : Complex) (maxSteps : Nat) : Array (Array Nat) := do
   let mut scene : Array (Array Complex) := twoDeeRange (Complex.mk 0 0) px py
@@ -37,7 +66,6 @@ def newScene (px py : Nat) (cMin cMax : Complex) (maxSteps : Nat) : Array (Array
 open Float in
   def Float.clampToUInt8 (x : Float) : UInt8 :=
     toUInt8 <| min 255 <| max 0 x
-
 
 def IO.FS.Handle.writeColor (handle : IO.FS.Handle) (cMax : Nat) (c : Nat) : IO Unit := do
   let r := Float.clampToUInt8 (Float.ofNat c / (Float.ofNat cMax) * 255)
